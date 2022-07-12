008d0da709f946b99baf297b6f910075380efde1
kenny-lopez <kennylopezgallardo@gmail.com>
2022-07-12 16:06:18 -0500

    updated

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 function hiddenA () {
     document.querySelector(".tarot-container-a").style.display = "none";
+    document.querySelector(".tarot-container-b").classList.remove("displayHidden");
 }
+
