ae67ebd21933a9edf3a3dc75a7bf2c1577293410
kenny-lopez <kennylopezgallardo@gmail.com>
2022-07-12 16:10:54 -0500

    updated

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+window.addEventListener("load",function() {
+    setTimeout(function(){
+        // This hides the address bar:
+        window.scrollTo(0, 1);
+    }, 0);
+});
+
 function hiddenA () {
     document.querySelector(".tarot-container-a").style.display = "none";
     document.querySelector(".tarot-container-b").classList.remove("displayHidden");
