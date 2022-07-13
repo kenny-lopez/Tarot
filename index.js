966d8dee0407e1daf230a7bc4baa868898ca41d6
kenny-lopez <kennylopezgallardo@gmail.com>
2022-07-12 21:29:32 -0500

    Update

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 function hiddenA () {
-    document.querySelector(".tarot-container-a").style.display = "none";
+    document.querySelector(".tarot-container-a").classList.add("displayHidden");
     document.querySelector(".tarot-container-b").classList.remove("displayHidden");
 }
 
 function hiddenBTarotDaily () {
-    document.querySelector(".tarot-container-b").style.display = "none";
+    document.querySelector(".tarot-container-b").classList.add("displayHidden");
     document.querySelector(".tarot-container-daily").classList.remove("displayHidden");
 }
 
 function hiddenDailyToResult () {
-    document.querySelector(".tarot-container-daily").style.display = "none";
+    document.querySelector(".tarot-container-daily").classList.add("displayHidden");
     document.querySelector(".tarot-container-daily-result").classList.remove("displayHidden");
+}
+
+function returnToB () {
+    document.querySelector(".tarot-container-daily-result").classList.add("displayHidden");
+    document.querySelector(".tarot-container-b").classList.remove("displayHidden");
 }
